3dba0882575b76299afd9a5cd14d4d61a20ed9e3
Georgy Firsov <gfirsov007@gmail.com>
2023-02-11 21:44:25 +0300

    fix: output format improved

--- cryptoproviders/enum-provider-types/main.cpp
+++ cryptoproviders/enum-provider-types/main.cpp
@@ -76,7 +76,7 @@ int wmain()
             // Well, here I need just to print the type in a pretty way
             //
 
-            std::wcout << std::format(LR"(Provider type: {:4} "{}")", provider_type, type_name) << L'\n';
+            std::wcout << std::format(LR"(Provider type: {:4}; type name: "{}")", provider_type, type_name) << L'\n';
         }
 
         return 0;
